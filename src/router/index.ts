825fc02db35eb519243bffe994d3c6d0a76ec5a7
Abdulrahman <movieshome160@gmail.com>
2026-06-10 14:09:31 +0300

    feat: implement login portal view with useApi and Pinia state

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import authRoutes from "./auth.routes";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      redirect: "/login",
+    },
+    ...authRoutes,
+  ],
+});
 
-export default router
+export default router;
