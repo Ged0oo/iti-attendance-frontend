f44a0fe55563da7781b75200633a6aaebba7b9c9
Ged0oo <mohmmednagy@gmail.com>
2026-06-11 22:46:54 +0300

    solve bug in auth routers

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -3,5 +3,9 @@ export default [
     path: '/login',
     name: 'login',
     component: () => import('../views/auth/LoginView.vue'),
+    meta: {
+      requiresAuth: false,
+      title: "Login - ITI Attendance & Grading",
+    },
   },
 ]
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import attendanceRoutes from './attendance.routes.js'
 import gradingRoutes from './grading.routes.js'
 import excuseRoutes from './excuse.routes.js'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -17,6 +18,7 @@ const router = createRouter({
     ...attendanceRoutes,
     ...gradingRoutes,
     ...excuseRoutes,
+
   ],
 })
 
